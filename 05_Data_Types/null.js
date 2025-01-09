489a6d5292ed27a60ce1cfca034cebf5a354a070
Pop2323 <omar4500455@gmail.com>
2025-01-09 20:26:34 +0200

    null

---
+++ 05_Data_Types/null.js
@@ -0,0 +1,5 @@
+// Null represents "nothing", "empty" or "value unknown".
+
+let nullVariable = null;
+console.log(nullVariable); // Outputs: null
+console.log(typeof nullVariable); // Outputs: object (this is a well known bug in JavaScript, typeof operator is not working correctly there)
