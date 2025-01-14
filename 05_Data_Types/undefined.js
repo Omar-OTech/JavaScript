efff6b544eb34c0dc7ee713327ed1ba7e257f39b
Pop2323 <omar4500455@gmail.com>
2025-01-14 18:12:47 +0200

    Undefind

---
+++ 05_Data_Types/undefined.js
@@ -0,0 +1,5 @@
+// A variable that has been declared but hasn't been assigned a value is of type undefined.
+
+let undefinedVariable;
+console.log(undefinedVariable); // Outputs: undefined
+console.log(typeof undefinedVariable); // Outputs: undefined
