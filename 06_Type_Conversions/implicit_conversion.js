eb4399c5335f1fe5b3ee1cd626beab9aa4229401
Pop2323 <omar4500455@gmail.com>
2025-01-14 18:13:36 +0200

    implicit_conversion

---
+++ 06_Type_Conversions/implicit_conversion.js
@@ -0,0 +1,12 @@
+// String concatenation
+console.log("3" + 4); // Outputs: "34"
+console.log(3 + "4"); // Outputs: "34"
+console.log("3" + true); // Outputs: "3true"
+
+// Numeric conversion
+console.log("6" - 2); // Outputs: 4
+console.log("6" * "2"); // Outputs: 12
+
+// Equality operators
+console.log("5" == 5);       // true
+console.log("5" === 5);      // false
