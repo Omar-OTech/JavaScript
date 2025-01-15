ad274c3ac328726e20d188414eff1c70d1236957
Pop2323 <omar4500455@gmail.com>
2025-01-15 23:30:09 +0200

    to_boolean

---
+++ 06_Type_Conversions/to_boolean.js
@@ -0,0 +1,7 @@
+// Using Boolean() function
+console.log(Boolean(123)); // Outputs: true
+console.log(Boolean(0)); // Outputs: false
+console.log(Boolean("Hello")); // Outputs: true
+console.log(Boolean("")); // Outputs: false
+console.log(Boolean(null)); // Outputs: false
+console.log(Boolean(undefined)); // Outputs: false
