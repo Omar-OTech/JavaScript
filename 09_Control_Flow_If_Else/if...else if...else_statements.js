df442a8ee1bf5fc2e094ab76e0ecd35d9408fa13
Pop2323 <omar4500455@gmail.com>
2025-01-23 16:10:22 +0200

    if_else_Statement

---
+++ 09_Control_Flow_If_Else/if...else if...else_statements.js
@@ -0,0 +1,11 @@
+let score = 85;
+
+    if (score >= 90) {
+        console.log("A grade");
+    } else if (score >= 80) {
+        console.log("B grade");
+    } else if (score >= 70) {
+        console.log("C grade");
+    } else {
+        console.log("Needs improvement");
+}
