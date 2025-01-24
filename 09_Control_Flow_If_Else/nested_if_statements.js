06aaffbc812a2f20cfdbe61e5ffb097c5411d469
Pop2323 <omar4500455@gmail.com>
2025-01-24 18:02:57 +0200

    Nested_if_statements

---
+++ 09_Control_Flow_If_Else/nested_if_statements.js
@@ -0,0 +1,12 @@
+let num = 15;
+
+if (num > 0) {
+    console.log("Number is positive");
+    if (num % 2 === 0) {
+        console.log("Number is even");
+    } else {
+        console.log("Number is odd");
+    }
+} else {
+    console.log("Number is zero or negative");
+}
