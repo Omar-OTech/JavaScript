00b7edaf15653a9137ad64b64dd26c3029c6b199
Pop2323 <omar4500455@gmail.com>
2025-02-11 00:26:48 +0200

    Functions

---
+++ 12_Functions/functions.js
@@ -0,0 +1,10 @@
+function functionName(parameter1, parameter2) {
+    // function body
+    // code to be executed
+}
+
+function sayHello() {
+    console.log('Hello World!');
+}
+
+sayHello(); // Hello World!
