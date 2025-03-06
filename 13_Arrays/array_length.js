d6bb75dc2bfdebf0327400744853131b21a6462e
Pop2323 <omar4500455@gmail.com>
2025-03-06 14:36:26 +0200

    array_length

---
+++ 13_Arrays/array_length.js
@@ -0,0 +1,7 @@
+// Arrays have a length property that tells us how many elements are in the array:
+let colors = ['red', 'green', 'blue'];
+console.log(colors.length); // 3
+
+// Length updates automatically when we modify the array
+colors[3] = 'black';
+console.log(colors.length); // 4
